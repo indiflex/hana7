1fec03d323a7260a0c2343ad8ad84807e9e881ce
indiflex <indiflex1@gmail.com>
2025-04-03 16:15:18 +0900

    scope

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+console.log('Hello, JavaScript!');
+
+const 내이름 = 'Sico';
+
+function printName(nm) {
+  console.log(1, nm);
+
+  // const myName = 'Hanaro';
+  {
+    var myName = 'True';
+    console.log(22, myName);
+  }
+  console.log(2, myName);
+}
+printName(내이름);
+console.log(3, 내이름);
