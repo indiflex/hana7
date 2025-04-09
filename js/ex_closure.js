2e284a791195a8865d9f37ca89cfe35b54e8a8eb
indiflex <indiflex1@gmail.com>
2025-04-10 08:59:04 +0900

    makeArray & makeArrayTCO

--- js/ex_closure.js
+++ js/ex_closure.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 // [1,2,3,4,5,6,7, 8, 9, 10]
 function makeArray(n) {
   if (n === 1) return [1];
@@ -10,14 +12,18 @@ function makeReverseArray(n) {
 
 const ma10 = makeArray(10);
 console.log('🚀 ma10:', ma10);
+assert.deepEqual(ma10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+assert.equal(ma10[0], '1');
+assert.strictEqual(ma10[0], 1);
 
 const mra5 = makeReverseArray(5);
-console.log('🚀 mra5:', mra5);
+// console.log('🚀 mra5:', mra5);
+assert.deepEqual(mra5, [5, 4, 3, 2, 1], 'reverse fail!!');
 
 const maTco10 = makeArrayTCO(10);
 console.log('🚀 maTco10:', maTco10);
 
-function makeArrayTCO(n, acc = [9, 10]) {
+function makeArrayTCO(n, acc = []) {
   if (n === 1) return [1, ...acc];
 
   return makeArrayTCO(n - 1, [n, ...acc]);
