ef854953126adf85e03139db4b1bbc2c3eaada17
indiflex <indiflex1@gmail.com>
2025-04-09 13:26:46 +0900

    closure

--- js/strict.js
+++ js/strict.js
@@ -1,7 +1,23 @@
 'use strict';
+var x;
 function f(a, aa) {
-  console.log('outer f', a);
+  console.log('new.target>>', new.target, arguments);
+  console.log('outer f', a, x, this?.id);
+  // const fff = () => a;
+  function ff() {
+    return a;
+  }
+  return ff;
 }
+const xxx = f(10000);
+xxx();
+
+// function xf() {}
+let xf = () => {};
+xf = a => a ** 2;
+xf(3);
+
+console.log(this === globalThis);
 
 // f = 1;
 // NaN = 1;
@@ -11,6 +27,16 @@ function f(a, aa) {
   function f(a) {
     console.log('block f', a);
   }
-  f(100);
+  const c = 1;
+  var v = 0;
 }
-f(200);
+const u = { id: 1 };
+f.bind(u)(200);
+const bf = f.bind({ id: 300 });
+
+bf(1);
+bf(2);
+new f(300);
+console.log('v=', v);
+
+f.apply({ id: 500 }, 5);
