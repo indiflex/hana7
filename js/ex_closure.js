1331814c63921c52002a34459463a22d92aef10e
indiflex <indiflex1@gmail.com>
2025-04-10 10:17:49 +0900

    fibonacci - memoization

--- js/ex_closure.js
+++ js/ex_closure.js
@@ -28,3 +28,51 @@ function makeArrayTCO(n, acc = []) {
 
   return makeArrayTCO(n - 1, [n, ...acc]);
 }
+console.log('-------------------');
+
+function loopFibonacci(n) {
+  if (n <= 1) return n;
+  let prev = 0;
+  let curr = 1;
+  for (let i = 2; i <= n; i += 1) {
+    // let t = prev;
+    // prev = curr;
+    // curr = t + curr;
+    [prev, curr] = [curr, prev + curr];
+  }
+
+  return curr;
+}
+
+function recurFibonacci(n) {
+  if (n <= 1) return n;
+  return recurFibonacci(n - 2) + recurFibonacci(n - 1);
+}
+
+const memoFibonacci = memoized(function (n) {
+  if (n <= 1) return n;
+  return memoFibonacci(n - 2) + memoFibonacci(n - 1);
+});
+
+const lf3 = loopFibonacci(3);
+console.log('🚀 lf3:', lf3);
+const lf5 = loopFibonacci(5); // 5
+console.log('🚀 lf5:', lf5);
+const lf7 = loopFibonacci(7); // 13
+console.log('🚀 lf7:', lf7);
+console.log('---------------- recursive');
+assert.equal(recurFibonacci(3), 2);
+assert.equal(recurFibonacci(5), 5);
+assert.equal(recurFibonacci(7), 13);
+assert.equal(recurFibonacci(30), 832040);
+
+assert.equal(memoFibonacci(5), 5);
+assert.equal(memoFibonacci(7), 13);
+assert.equal(memoFibonacci(30), 832040);
+
+function memoized(fn) {
+  const cache = {};
+  return function (k) {
+    return cache[k] || (cache[k] = fn(k));
+  };
+}
