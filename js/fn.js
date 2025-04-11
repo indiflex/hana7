cb64dd8af9a83d2378f13ad946010de1ffee1ab0
indiflex <indiflex1@gmail.com>
2025-04-11 09:44:14 +0900

    function call type and this

--- js/fn.js
+++ js/fn.js
@@ -12,7 +12,7 @@ const af = () => {
 };
 af();
 
-const obj = {
+let obj = {
   name: 'ObjName',
   bark() {
     // good!(호출한 객체)
@@ -20,23 +20,26 @@ const obj = {
   },
   bark2: () =>
     // bad!! ==> this=전역(browser)/module(node)
-    console.log('bark2=', this.name),
+    console.log('bark2=', this.name, self.name),
 };
 
 obj.bark();
 obj.bark2();
+const ob = obj.bark;
+obj = null;
+ob();
 
-const expressFn = function(name) {
+const expressFn = function (name) {
   'use strict';
   // if(this?.name)
   // this.name = name;
   console.log(new.target, this.name, name, this instanceof expressFn);
-}
+};
 
-const arrowFn = (name) => {
+const arrowFn = name => {
   this.name = name;
-  console.log('-->', this, new.target, this.name, name);
-}
+  console.log('-->', this, this.name, name);
+};
 
 // expressFn('expfn');
 expressFn.bind({})('expfn');
@@ -54,17 +57,15 @@ const dfn = new expressFn('D');
 console.log('🚀 dfn:', Object.getPrototypeOf(dfn));
 // const afn = new arrowFn('A'); // error!
 
-console.log('-------------------------------')
-const Dog = function(name) {
-  console.log(name, '==>', this.name, new.target, 
-              this instanceof Dog);
+console.log('-------------------------------');
+const Dog = function (name) {
+  console.log(name, '==>', this.name, new.target, this instanceof Dog);
   this.name = name;
   this.bark = function () {
     console.log('bark=', new.target, this.name, name);
   };
-  this.bark2 = () =>
-    console.log('bark2=', new.target, this.name, name);
-}
+  this.bark2 = () => console.log('bark2=', new.target, this.name, name);
+};
 
 const dog = Dog('Doggy');
 const lucy = new Dog('Lucy');
@@ -73,3 +74,28 @@ lucy.bark(); // ?
 lucy.bark2(); // ?
 console.log('dog type=', typeof dog); // ?
 console.log('lucy type=', typeof lucy); // ?
+
+console.log('****************************');
+this.name = 'Module Name';
+globalThis.name = 'GlobalName';
+const Cat = name => {
+  console.log('Cat>>', this);
+  this.name = name;
+
+  this.bark = function () {
+    console.log('bark=', new.target, this.name, name);
+  };
+
+  this.bark2 = () => console.log('bark2=', this.name, name);
+
+  return this;
+};
+
+const cat = Cat('Coco');
+// const cat = new Cat(''); // error!!
+cat.bark(); // ?
+// cat.bark2(); // ?
+// Cat().bark(); // ?
+// console.log('type=', typeof cat); // ?
+
+// cf. FunctionEnvironmentRecord.[[ThisValue]]
