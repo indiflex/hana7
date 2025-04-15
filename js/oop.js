213197c2adc8398323a17d9deffae6417c8912e6
indiflex <indiflex1@gmail.com>
2025-04-15 14:58:25 +0900

    singleton & proxy

--- js/oop.js
+++ js/oop.js
@@ -1,8 +1,36 @@
+'use strict';
 class Animal {
   static ID = 1;
+  constructor(name) {
+    this.name = name;
+  }
 }
 
 class Dog extends Animal {
+  #age = 0;
+  constructor(name, age) {
+    super(name);
+    // this.name = name;
+    this.#age = age;
+  }
+
+  setName(newName) {
+    this.name = newName;
+  }
+
+  getAge() {
+    return this.#age;
+  }
+
+  get age() {
+    return this.#age;
+  }
+
+  set age(newAge) {
+    console.log('🚀 newAge---:', newAge, this.age);
+    this.#age = newAge;
+  }
+
   f() {
     return this.ID;
   }
@@ -15,5 +43,57 @@ const ani = new Animal();
 const aid = ani.ID;
 console.log('🚀 aid:', aid);
 
-const dog = new Dog();
+const dog = new Dog('Dog', 3);
 console.log('🚀 dog:', dog.f(), Dog.sf());
+console.log('🚀 name:', dog.name, dog.getAge());
+dog.setName('Maxx');
+// dog.name = 'Maxx';
+dog.age = 9;
+console.log('🚀 name:', dog.name, dog.age);
+
+// ----------------------
+function wrapFullname(user) {
+  return new Proxy(user, {
+    get(target, prop) {
+      // log(target, prop, loginEmp)
+      if (prop === 'fullname') {
+        return `${target.firstName} ${target.lastName}`;
+      } else {
+        return target[prop];
+      }
+    },
+
+    set(target, prop, value) {
+      if (prop === 'fullname') {
+        [target.firstName, target.lastName] = value.split(' ');
+      } else {
+        target[prop] = value;
+      }
+      return target;
+    },
+  });
+}
+const hongOrg = {
+  id: 1,
+  firstName: 'Kildong',
+  lastName: 'Hong',
+  // get fullname() {
+  //   return `${this.firstName} ${this.lastName}`;
+  // },
+  // set fullname(fname) {
+  //   console.log('🚀 fname:', fname);
+  //   [this.firstName, this.lastName] = fname.split(' ');
+  // },
+};
+
+const hong = wrapFullname(hongOrg);
+
+console.log('🚀 fullname11:', hong.id, hong.fullname);
+hong.fullname = 'Jhon Hong';
+console.log('🚀 fullname22:', hong.fullname);
+
+const kim = wrapFullname({ id: 2, firstName: 'Kildong', lastName: 'Kim' });
+console.log('🚀 kim:', kim.id, kim.fullname);
+kim.id = 5;
+kim.fullname = 'Jhon Kim';
+console.log('🚀 kim:', kim.id, kim.fullname);
