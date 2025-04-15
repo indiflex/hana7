4e0cb0f3b9312e9c928215f837832c13e3ab5a76
indiflex <indiflex1@gmail.com>
2025-04-15 10:51:26 +0900

    array ex - range

--- js/ex_array2.js
+++ js/ex_array2.js
@@ -83,3 +83,57 @@ const xr4 = arr.map(a =>
   [square, cube, n => n + 1].reduce((acc, fn) => fn(acc), a)
 );
 console.log('🚀  xr4:', xr4);
+
+// ------------------------
+const range = (start, end, step = start > end ? -1 : 1) => {
+  if (step === 0 || start === end) return [start];
+  if ((start - end) * step > 0) return [];
+  if (end === undefined && start === 0) return [0];
+
+  const t = start;
+  end = end ?? (start > 0 ? ((start = 1), t) : -1);
+
+  const results = [];
+  for (let i = start; start > end ? i >= end : i <= end; i += step) {
+    results.push(i);
+  }
+
+  return results;
+};
+
+assert.deepStrictEqual(range(1, 10, 1), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+assert.deepStrictEqual(range(1, 10, 2), [1, 3, 5, 7, 9]);
+assert.deepStrictEqual(range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+assert.deepStrictEqual(range(10, 1), [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+
+assert.deepStrictEqual(range(5, 5, 0), [5]);
+assert.deepStrictEqual(range(1, 5, 0), [1]);
+assert.deepStrictEqual(range(5, 5, -1), [5]);
+assert.deepStrictEqual(range(5, 5), [5]);
+assert.deepStrictEqual(range(0, 0, 5), [0]);
+assert.deepStrictEqual(range(1, 5, -1), []);
+
+assert.deepStrictEqual(range(1, 5, 6), [1]);
+assert.deepStrictEqual(range(0, 5), [0, 1, 2, 3, 4, 5]);
+assert.deepStrictEqual(range(-3, 0), [-3, -2, -1, 0]);
+
+assert.deepStrictEqual(range(5, 1, 1), []);
+assert.deepStrictEqual(range(0, -1), [0, -1]);
+assert.deepStrictEqual(range(0, -3), [0, -1, -2, -3]);
+assert.deepStrictEqual(range(5, 1), [5, 4, 3, 2, 1]);
+assert.deepStrictEqual(range(10, 1, -2), [10, 8, 6, 4, 2]);
+
+assert.deepStrictEqual(range(5), [1, 2, 3, 4, 5]);
+assert.deepStrictEqual(range(0), [0]);
+assert.deepStrictEqual(range(0, 0), [0]);
+assert.deepStrictEqual(range(2, 1, -5), [2]);
+assert.deepStrictEqual(range(0, -1, -5), [0]);
+assert.deepStrictEqual(range(-5), [-5, -4, -3, -2, -1]);
+assert.deepStrictEqual(
+  range(50),
+  Array.from({ length: 50 }, (_, i) => i + 1)
+);
+assert.deepStrictEqual(
+  range(1, 150, 3),
+  Array.from({ length: 50 }, (_, i) => i * 3 + 1)
+);
