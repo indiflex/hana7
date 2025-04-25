6e20d60ecb8c77ab188e2c3cd39e2a262b7debf1
indiflex <indiflex1@gmail.com>
2025-04-25 11:33:20 +0900

    mapped

--- ts/dist/obj.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let hong = { id: 1, name: 'Hong' };
-hong = Object.assign({ id: 1 }, { name: 'Kim' });
-// hong = { id: 2, name: 'Kim', addr: 'Seoul' };
-hong = { id: 2, name: 'Kim', addr: 'Seoul' };
-const hongx = { id: 2, name: 'Kim', addr: 'Seoul' };
-hong = hongx;
