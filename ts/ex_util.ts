b7208b1232f891ccae9c8b2adce55e3e0ddfdd09
indiflex <indiflex1@gmail.com>
2025-04-28 13:35:34 +0900

    example of utility types

--- ts/ex_util.ts
+++ ts/ex_util.ts
@@ -15,6 +15,8 @@ type Combine<T, U> = {
   [k in keyof (T & U)]: k extends keyof T & keyof U ? T[k] | U[k] : (T & U)[k];
 };
 
+// type CombineExclude<T, U, E> = Exclude<Combine<T, U>, E>;
+
 // type CombineExclude<T, U, E extends keyof (T & U)> = {
 //   [k in Exclude<keyof Combine<T, U>, E>]: (T & U)[k];
 // }
@@ -29,5 +31,47 @@ let combineExclude: ICombineExclude = {
   age: 33,
   captain: 'ccc',
 };
+
+// -------------------------------------------
+
+function registUserObj({ name, age }: { name: string; age: number }) {
+  const id = 100;
+  return { id, name, age };
+}
+
+type RegistUserObj = Parameters<typeof registUserObj>[number];
+
+const paramObj: RegistUserObj = { name: 'Hong', age: 32 };
+const newUser2 = registUserObj(paramObj);
+console.log('🚀  newUser2:', newUser2);
+
+// -------------------------------------------
+const debounce = <T extends (...args: Parameters<T>) => ReturnType<T>>(
+  cb: T,
+  delay: number = 1
+) => {
+  let timer: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<typeof cb>) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(cb, delay, ...args);
+  };
+};
+
+const throttle = <T>(cb: (...args: T[]) => any, delay: number = 1) => {
+  let timer: ReturnType<typeof setTimeout> | null;
+  return (...args: T[]) => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      cb(...args);
+      timer = null;
+    }, delay);
+  };
+};
+
+const debo = debounce((a: number, b: string) => console.log(a + 1, b), 1000);
+for (let i = 10; i < 15; i++) debo(i, 'abc'); // 15, 'abc'
+
+const thro = throttle((a: number) => console.log(a + 1), 1000);
+for (let i = 10; i < 15; i++) thro(i); // 11
 
 export {};
