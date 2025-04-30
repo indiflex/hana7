51008459b75453fb9e369736ca7ac58542df8697
indiflex <indiflex1@gmail.com>
2025-04-30 17:47:27 +0900

    join update example

--- sql/ex_Emp_captain_EmailLog.sql
+++ sql/ex_Emp_captain_EmailLog.sql
@@ -21,3 +21,43 @@ create table EmailLog (
   foreign key fk_EmailLog_sender_Emp (sender)
     references Emp(id) on update cascade on delete no action
 ) ENGINE = MyIsam;
+
+select * from Emp;
+
+select e.dept, avg(salary) avgsal from Emp e
+ group by dept having avgsal > (select avg(salary) from Emp);
+ 
+select dept, max(ename), max(salary) from Emp group by dept;
+select * from Emp where salary = 901 order by dept, id;
+select * from Dept;
+
+select * from Emp where id in (3,5,14,26);
+select e1.*, e2.id
+  from Emp e1 left join Emp e2 on e1.dept = e2.dept and e1.ename > e2.ename
+ where e2.id is null order by e1.dept;
+
+select * from Emp where ename = SOME(select min(ename) from Emp group by dept)
+ order by dept;
+ 
+select * from Emp where salary > SOME(select min(ename) from Emp group by dept)
+ order by dept;
+
+select e1.*, e2.id
+  from Emp e1 left join Emp e2 on e1.dept = e2.dept and e1.salary >= e2.salary
+ where e2.id is null order by e1.dept;
+
+select e1.dept, e2.dname, e1.id, e1.ename, e1.salary
+  from Emp e1 join (select dept, d.dname, max(salary) salary from Emp e inner join Dept d on e.dept = d.id group by dept) e2
+    on e1.dept = e2.dept and e1.salary = e2.salary
+ order by e1.dept;
+    
+select * from Dept;
+
+
+
+select e1.*, e2.id, e2.ename
+  from Emp e1 left join Emp e2 on e1.dept = e2.dept and e1.salary < e2.salary
+ where e2.id is null
+       order by e1.dept;
+       
+       
