6b5f377b23e1acf566d9a017f81e8b50ff0ba349
indiflex <indiflex1@gmail.com>
2025-05-01 10:34:31 +0900

    update Emp.outdt with Dept.captain

--- sql/testdb_examples.sql
+++ sql/testdb_examples.sql
@@ -31,9 +31,35 @@ update Dept d
   set d.captain = sub.id
  where d.id > 0;
   
-select * from Dept where captain is null;
+select * from Dept where captain is not null;
 
 select *, (select ename from Emp where id = d.captain) ename from Dept d;
 
--- 
 select d.*, e.ename from Dept d inner join Emp e on d.id = e.dept and d.captain = e.id;
+
+-- ex) Emp.outdt set with captain make null
+alter table Emp add column outdt date comment '퇴사일';
+alter table Emp add column outdt2 varchar(10) comment '퇴사일2';
+
+-- update Emp set outdt = CAST('2025-04-25' as date), outdt2 = '2025-04-25'
+update Emp set outdt = '2025-04-26', outdt2 = '2025-04-25'
+ where id in (3, 5);
+ 
+select *
+  from Emp e left outer join Dept d on e.id = d.captain
+ where e.id in (14, 26);
+ 
+update Emp set outdt = null, outdt2 = null where id in (14, 26);
+ 
+start transaction; -- conn.setAutoCommit(false), conn.beginTransaction();
+
+update Emp e left outer join Dept d on e.id = d.captain
+   set e.outdt = curdate(), e.outdt2 = curdate(),
+       d.captain = null
+ where e.id in (14, 26);
+
+commit;
+rollback;
+
+select * from Emp where id in (3,5,14,26);
+select * from Dept;
