4fe78c6ba4397797ef19572fcee5dd3df3b70d3c
indiflex <indiflex1@gmail.com>
2025-05-07 09:56:47 +0900

    cte

--- sql/advance_sql.sql
+++ sql/advance_sql.sql
@@ -25,3 +25,24 @@ WITH AvgSal AS
  UNION
  select '평균 차액', ' - ', max(avgsal) - min(avgsal) from SumUp;
  
+select * from Dept where pid = 0;
+select * from Dept where pid in (1, 2);
+select * from Dept where pid in (3,4,5,6,7);
+
+insert into Dept(pid, dname) values(6, '인프라셀');
+insert into Dept(pid, dname) values(6, 'DB셀');
+insert into Dept(pid, dname) values(7, '모바일셀');
+
+select * from Dept d1 inner join Dept d2 on d1.id = d2.pid;
+
+WITH RECURSIVE CteDept(id, pid, pname, dname, dx, h) AS 
+(
+    select id, pid, cast('' as char(31)), dname, 0, cast(id as  char(10))
+      from Dept where pid = 0
+    UNION ALL
+    select d.id, d.pid, cte.dname, d.dname, dx + 1, concat(cte.h, '-', d.id) 
+      from Dept d inner join CteDept cte on d.pid = cte.id
+)
+select concat(repeat('↳', dx), dname), id, dname, h from CteDept order by h;
+
+show variables like '%max%';
