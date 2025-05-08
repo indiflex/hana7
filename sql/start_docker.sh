6b5d2f9c59bea52d6735d24693550afdc84267e2
indiflex <indiflex1@gmail.com>
2025-05-08 15:04:08 +0900

    start docker shell script

---
+++ sql/start_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker-compose up -d
+
+sleep 2
+
+mysql -uroot -pTestdbRoot -h 127.0.0.1 -P 3309 -e "SET GLOBAL innodb_optimize_fulltext_only = ON;"
+mysql -uroot -pTestdbRoot -h 127.0.0.1 -P 3309 -e "optimize table testdb.Notice;"
+mysql -uroot -pTestdbRoot -h 127.0.0.1 -P 3309 -e "SET GLOBAL innodb_optimize_fulltext_only = OFF;"
+mysql -uroot -pTestdbRoot -h 127.0.0.1 -P 3309 -e "set global innodb_ft_aux_table='testdb/Notice';"
