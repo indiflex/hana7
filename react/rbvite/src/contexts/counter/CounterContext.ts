5e42f6335ab81dac94c490e35660fef5a0683a45
indiflex <indiflex1@gmail.com>
2025-05-14 17:26:08 +0900

    useEffect examples

--- react/rbvite/src/contexts/counter/CounterContext.ts
+++ react/rbvite/src/contexts/counter/CounterContext.ts
@@ -3,9 +3,11 @@ import { createContext } from 'react';
 type CounterContextProps = {
   count: number;
   plusCount: () => void;
+  minusCount: () => void;
 };
 
 export const CounterContext = createContext<CounterContextProps>({
   count: 0,
   plusCount: () => {},
+  minusCount: () => {},
 });
