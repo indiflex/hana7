a6a76a03cfc1d9f0786fa9bfdb767a0934ab9bb4
indiflex <indiflex1@gmail.com>
2025-05-22 16:25:09 +0900

    Suspense, generateStaticParams

--- next/my-app/app/(playground)/hi/[time]/page.tsx
+++ next/my-app/app/(playground)/hi/[time]/page.tsx
@@ -5,8 +5,18 @@ type Props = {
   params: Promise<{ time: string }>;
 };
 
+const Times = ['morning', 'afternoon', 'Evening', 'Night'];
+export async function generateStaticParams() {
+  return Times.map((time) => ({ time }));
+}
+
 export default function Time({ params }: Props) {
   const { time } = use(params);
-  const [firstChar, ...timeStr] = time;
-  return <>Good {`${firstChar.toUpperCase()}${timeStr.join('')}`} ~</>;
+  return (
+    <>
+      Good <span className='capitalize'>{time}</span>
+    </>
+  );
+  // const [firstChar, ...timeStr] = time;
+  // return <>Good {`${firstChar.toUpperCase()}${timeStr.join('')}`} ~</>;
 }
