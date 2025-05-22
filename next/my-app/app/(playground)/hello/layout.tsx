d4db4bc4d072ad2d3114d7c6a071c60ea83bae62
indiflex <indiflex1@gmail.com>
2025-05-22 15:37:13 +0900

    route

--- next/my-app/app/(playground)/hello/layout.tsx
+++ next/my-app/app/(playground)/hello/layout.tsx
@@ -1,27 +1,45 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { PropsWithChildren, useLayoutEffect, useState } from 'react';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { PropsWithChildren, Suspense, useLayoutEffect, useState } from 'react';
 
 export default function HelloLayout({ children }: PropsWithChildren) {
   const [state, setState] = useState('');
   const sparams = useSearchParams();
-  console.log('🚀 sparams:', sparams.get('q'));
+  // console.log('🚀 sparams:', sparams.get('q'));
   useLayoutEffect(() => {
     setState(sparams.get('q') ?? '');
   }, [sparams]);
 
+  const router = useRouter();
+  const pathname = usePathname();
+  console.log('🚀 pathname:', pathname);
   const setSearchParams = () => {
     const urlParams = new URLSearchParams(sparams.toString());
-    console.log('*******');
-    urlParams.set('q', 'new-qqq');
+    urlParams.set('q', `${new Date()}`); // q=****
+    urlParams.set('r', `rrr`);
+    router.push(`/hello?${urlParams.toString()}`);
   };
 
   return (
     <>
       <h1>This is Hello Layout: {state}</h1>
+      <ul className='flex gap-3'>
+        <li>
+          <Link href='/hello/morning'>Morning</Link>
+        </li>
+        <li>
+          <Link href='/hello/afternoon'>Afternoon</Link>
+        </li>
+        <li>
+          <Link href='/hello/evening'>Evening</Link>
+        </li>
+      </ul>
       <hr />
-      <div>{children}</div>
+      <Suspense>
+        <div>{children}</div>
+      </Suspense>
       <button onClick={setSearchParams}>SetParam</button>
     </>
   );
