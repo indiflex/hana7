09596590091ed632c8db4ddeb95913cb2efdf94e
indiflex <indiflex1@gmail.com>
2025-05-23 13:58:36 +0900

    hi/[time] intercepting routes

--- next/my-app/app/intercept/(.)ic1/page.tsx
+++ next/my-app/app/intercept/(.)ic1/page.tsx
@@ -1,4 +1,10 @@
+import Modal from '../../components/Modal';
+
 export default function Ic1Intercept() {
-  console.log('ic1 !');
-  return <>Ic1-Intercept</>;
+  return (
+    <>
+      Ic1-Intercept
+      <Modal>This is Modal!!</Modal>
+    </>
+  );
 }
