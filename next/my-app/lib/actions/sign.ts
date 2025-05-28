855071a3e3849068999ba05ec4ee977b77a6764d
indiflex <indiflex1@gmail.com>
2025-05-28 16:56:10 +0900

    callbackUrl & signIn

--- next/my-app/lib/actions/sign.ts
+++ next/my-app/lib/actions/sign.ts
@@ -6,6 +6,13 @@ export const login = async () => {
   await signIn('kakao', { redirectTo: '/' });
 };
 
+export const snsLogin = async (
+  service: string = 'kakao',
+  redirectTo: string = '/'
+) => {
+  await signIn(service, { redirectTo });
+};
+
 export const logout = async () => {
   await signOut({ redirectTo: '/api/auth/signin' });
 };
