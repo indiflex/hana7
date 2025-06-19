28725cc7527dca1d52b3642ccf29ac5f6034a514
indiflex <indiflex1@gmail.com>
2025-06-19 11:41:17 +0900

    내 정보 수정 및 파일 업로드

--- next/my-app/app/my/page.tsx
+++ next/my-app/app/my/page.tsx
@@ -2,8 +2,9 @@
 
 import { ShieldUserIcon } from 'lucide-react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
+import { useActionState } from 'react';
 import { updateUser } from '@/lib/actions/auth-actions';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -14,30 +15,48 @@ export default function My() {
 
   const router = useRouter();
   if (!data?.user?.id) {
-    router.push('/auth/signin');
-    return;
+    redirect('/auth/signin');
+    // router.push('/auth/signin');
+    // return;
   }
 
-  const {
-    user: { id, name, email, image, isadmin },
-  } = data;
+  const makeNameRequired = ({
+    id,
+    name,
+    email,
+    image,
+    isadmin,
+  }: typeof data.user) => ({
+    id: Number(id),
+    name: name ?? '',
+    email,
+    image,
+    isadmin,
+  });
 
-  const updateMyInfo = async (formData: FormData) => {
-    formData.append('id', id!);
-    formData.append('image', image || '');
-    try {
-      const userInfo = await updateUser(formData);
-      await update(userInfo);
-    } catch (err) {
-      if (err instanceof Error && 'message' in err) alert(err.message);
-      else alert(JSON.stringify(err));
-    }
-  };
+  const [user, updateMyInfo, isPending] = useActionState(
+    async (_pre: unknown, formData: FormData) => {
+      formData.append('id', data.user.id!);
+      formData.append('image', image || '');
+      try {
+        const userInfo = await updateUser(formData);
+        await update(userInfo);
+        router.refresh();
+        return userInfo;
+      } catch (err) {
+        if (err instanceof Error && 'message' in err) alert(err.message);
+        else alert(JSON.stringify(err));
+      }
+    },
+    makeNameRequired(data.user)
+  );
+
+  const { id, name, email, image, isadmin } = user!;
 
   return (
     <div className='w-[80%] mx-auto'>
       <h1 className='text-2xl flex justify-center gap-3'>
-        내 정보 수정 - {name} -{isadmin && <ShieldUserIcon />}
+        {id}. 내 정보 수정 - {name} -{isadmin && <ShieldUserIcon />}
       </h1>
 
       <div className='grid grid-cols-3 border p-5'>
@@ -54,10 +73,13 @@ export default function My() {
         </div>
         <form action={updateMyInfo} className='space-y-3 m-3 col-span-2'>
           <p className='leading-7'>{email}</p>
+          <p className='leading-7 text-sm'>{image}</p>
           {/* <Input type='hidden' name='id' defaultValue={id} /> */}
           <Input type='text' name='name' defaultValue={name!} />
-          <Input type='file' name='file' />
-          <Button type='submit'>Save</Button>
+          <Input type='file' name='file' accept='image/*' />
+          <Button type='submit' disabled={isPending}>
+            Save
+          </Button>
         </form>
       </div>
     </div>
