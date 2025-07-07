c95f2194c016f979ffe81b9cb3a5bb999b688ddd
indiflex <indiflex1@gmail.com>
2025-07-07 10:15:38 +0900

    ex4

---
+++ java/basic/src/Ex4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex4 {
+	public static void scoreToGrade(int score) {
+		char grade = switch (score / 10) {
+			case 10, 9 -> 'A';
+			case 8 -> 'B';
+			case 7 -> 'C';
+			default -> 'F';
+		};
+		System.out.println(grade);
+	}
+
+	// public static void gradeToMessage()
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Score/grade?");
+		// scoreToGrade(scanner.nextInt());
+		String msg = switch(scanner.next()) {
+			case "A", "B" -> "Good";
+			case "C", "D" -> "So so";
+			default -> "See u again";
+		};
+		System.out.println(msg);
+	}
+}
