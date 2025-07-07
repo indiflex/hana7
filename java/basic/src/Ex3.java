428abad4416d07c61d45e8f30c5b919f8cf11f54
indiflex <indiflex1@gmail.com>
2025-07-07 09:38:07 +0900

    ex3

---
+++ java/basic/src/Ex3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex3 {
+	final static int[] STIMMIES = {40, 60, 80, 100};
+	final static int UNIT = 10_000;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("가족 구성원 수는?");
+		int famSize = scanner.nextInt();
+		int stimmy;
+		if (famSize < 4) {
+			stimmy = STIMMIES[famSize - 1];
+		} else {
+			stimmy = STIMMIES[3];
+		}
+		System.out.printf("지원금: %,d원%n", stimmy * UNIT);
+	}
+}
