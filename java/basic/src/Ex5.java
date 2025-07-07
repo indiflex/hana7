977a7a61b4ec30c64306997a9747cafb4612c381
indiflex <indiflex1@gmail.com>
2025-07-07 11:38:24 +0900

    String.strip()

--- java/basic/src/Ex5.java
+++ java/basic/src/Ex5.java
@@ -8,5 +8,19 @@ public class Ex5 {
 			}
 			dan++;
 		}
+
+		String str = "   Hello World   ";
+		String result = str.trim();
+		System.out.println("'" + result + "'");    // 'Hello World'
+		String str2 = "\u2000\u2001  Hello World  \u2002\u2003";
+		System.out.println(":" + str2 + ":");
+		System.out.println("trim(): '" + str2.trim() + "'");     // 유니코드 공백 제거 안됨!
+		System.out.println("strip(): '" + str2.strip() + "'");   // 모든 공백 제거됨
+
+		char s = 33;
+		System.out.println(":" + s + ":");
+
+		String s1 = new String("Abcdefg");
+		System.out.println(s1.replace("cde", "CDE"));
 	}
 }
