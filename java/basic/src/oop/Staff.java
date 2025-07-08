b43bbc33b3bdbf981f6c35928372f6dda09a7806
indiflex <indiflex1@gmail.com>
2025-07-08 14:42:33 +0900

    enum

--- java/basic/src/oop/Staff.java
+++ java/basic/src/oop/Staff.java
@@ -1,5 +1,7 @@
 package oop;
 
+import java.util.Arrays;
+
 public class Staff extends Person {
 	private String school;
 	private double pay;
@@ -33,5 +35,68 @@ public class Staff extends Person {
 			", school='" + school + '\'' +
 			", pay=" + pay +
 			']';
+	}
+
+	public enum Code {
+		OK(200, "OK"),
+		Fail(500, "Error");
+
+		private final int code;
+		private final String msg;
+
+		private Code(int code, String msg) {
+			this.code = code;
+			this.msg = msg;
+		}
+
+		public boolean isSuccess() {
+			return code == 200 || msg.equals("OK");
+		}
+	}
+
+	public static void main(String[] args) {
+		enum Action {입금, 출금, 송금, 조회}
+		enum BloodType {A, B, AB, O}
+		BloodType[] types = BloodType.values();
+		System.out.println("types = " + Arrays.toString(types));
+		System.out.println(BloodType.A);
+		System.out.println(BloodType.B);
+		System.out.println("Action = " + Arrays.toString(Action.values()));
+
+		enum Gender {
+			MALE(1, "man"), FEMALE(2, "woman");
+
+			private final int gid;
+			private final String msg;
+
+			Gender(int gid, String msg) {
+				this.gid = gid;
+				this.msg = msg;
+			}
+
+			public boolean isMale() {
+				return this.gid == 1 || this.msg.equals("man");
+			}
+
+			public String scannerText() {
+				return this.name() + "(" + this.gid + ")";
+			}
+
+			@Override
+			public String toString() {
+				return "Gender{" +
+					"gid=" + gid +
+					", msg='" + msg + '\'' +
+					'}';
+			}
+		}
+
+		System.out.println(Arrays.toString(Gender.values()));
+
+		Gender g1 = Gender.MALE;
+		System.out.println(g1.isMale());
+		System.out.println(g1.scannerText());
+		Gender g2 = Gender.valueOf("FEMALE");
+		System.out.println("g2 = " + g2 + ", " + (g2 == Gender.FEMALE));
 	}
 }
