ac1b459e511cb3f204f96e46088ad336223fbbc5
indiflex <indiflex1@gmail.com>
2025-07-10 14:45:43 +0900

    generic example - AppShape

--- java/basic/src/oopex/ShapeMain.java
+++ java/basic/src/oopex/ShapeMain.java
@@ -2,24 +2,29 @@ package oopex;
 
 public class ShapeMain {
 	public static void main(String[] args) {
-		Shape[] shapes = {new Circle(3), new Rectangle(3, 4), new ResizableCircle(7), new ResizableRectangle(7, 8)};
+		Circle circle = new Circle(3);
+		ResizableCircle resizableCircle = new ResizableCircle(7);
+		AppShape<Circle> ac = new AppShape<>(circle);
 
-		for (Shape shape : shapes) {
+		// AppShape.safeResize(circle, 10);
+		AppShape.safeResize(resizableCircle, 10);
+		// AppShape.safeResize2(circle, 10);
+		AppShape.safeResize2(resizableCircle, 10);
+
+		AppShape<?>[] shapes = {
+			new AppShape<>(circle),
+			new AppShape<>(new Rectangle(3, 4)),
+			new AppShape<>(resizableCircle),
+			new AppShape<>(new ResizableRectangle(7, 8)),
+			new AppShape<>(new Triangle(4, 3))
+		};
+
+		for (AppShape<?> shape : shapes) {
+			shape.print();
 			try {
-				double perimeter = ((GeometricObject)shape).calcPerimeter();
-				System.out.printf("%n%s의 둘레는 %,.1f, 면적은 %,.1f%n", shape, perimeter, shape.calcArea());
-
-				int resizeRate = 10;
-				if (shape instanceof ResizableCircle || shape instanceof ResizableRectangle) {
-					System.out.printf("크기를 %d%% 변경 후%n", resizeRate);
-					Resizable resizableShape = (Resizable)shape;
-					resizableShape.resize(resizeRate);
-					double resizablePerimeter = ((GeometricObject)resizableShape).calcPerimeter();
-					System.out.printf("RESIZE: %s의 둘레는 %,.1f, 면적은 %,.1f%n", shape, resizablePerimeter,
-						shape.calcArea());
-				}
-			} catch (ClassCastException e) {
-				System.out.println(e.getMessage());
+				shape.resize(10);
+			} catch (IllegalStateException e) {
+				System.err.println(e.getMessage());
 			}
 		}
 	}
