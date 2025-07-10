2056e5226f5005a3a4cb2fb24d329989916f08f7
indiflex <indiflex1@gmail.com>
2025-07-10 10:59:13 +0900

    account with generic

--- java/basic/src/mybank/AppAccount.java
+++ java/basic/src/mybank/AppAccount.java
@@ -15,4 +15,22 @@ public class AppAccount<T extends Account> {
 		var benefit = (int)(amt * 0.1);
 		return this.account.deposit(amt + benefit);
 	}
+
+	public int withdraw(int amt) {
+		if (!isWithdrawable())
+			throw new IllegalStateException("Cannot withdraw: " + getAccountName());
+
+		return ((Withdrawable)account).withdraw(amt);
+	}
+
+	@Override
+	public String toString() {
+		return "AppAccount{" + getAccountName() +
+			"::balance=" + account.balance +
+			'}';
+	}
+
+	private String getAccountName() {
+		return this.account.getClass().getSimpleName();
+	}
 }
