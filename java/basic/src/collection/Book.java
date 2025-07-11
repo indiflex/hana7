9a41d015af83a0a2a1984fdfafee27e9a62c3706
indiflex <indiflex1@gmail.com>
2025-07-11 17:14:14 +0900

    Collection examples

--- java/basic/src/collection/Book.java
+++ java/basic/src/collection/Book.java
@@ -1,9 +1,14 @@
 package collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-public class Book {
+public class Book implements Comparable<Book> {
 	public Book(int id, String title, String author, int quantity) {
 		this.id = id;
 		this.title = title;
@@ -26,6 +31,31 @@ public class Book {
 			'}';
 	}
 
+	@Override
+	public int compareTo(Book otherBook) {
+		return Integer.compare(id, otherBook.id);
+	}
+
+	static void wordCount() {
+		Map<String, Integer> m = new HashMap<String, Integer>();
+		String[] sample = {"to", "be", "or", "not", "to", "be", "is", "a", "problem"};
+
+		for (String a : sample) {
+			if (!m.containsKey(a)) {
+				m.put(a, 1);
+				continue;
+			}
+
+			m.put(a, m.get(a) + 1);
+		}
+		System.out.println(m.size() + " 단어가 있습니다.");// 7
+		System.out.println(m.containsKey("to"));// true
+		if (Math.random() > 0.5)
+			m.clear();
+		System.out.println(m.isEmpty());// false
+		System.out.println(m);// {not=1, to=2, …}
+	}
+
 	public static void main(String[] args) {
 		Map<Integer, Book> map = new HashMap<>();
 		map.put(100, new Book(100, "어린왕자", "쌩떽쥐베리", 5));
@@ -33,11 +63,24 @@ public class Book {
 		map.put(5, new Book(300, "셜록홈즈", "코난도일", 10));
 
 		for (Map.Entry<Integer, Book> entry : map.entrySet()) {
-			int key = entry.getKey();
+			// int key = entry.getKey();
 			Book val = entry.getValue();
 			System.out.println(val);
 		}
 
 		System.out.println("map = " + map);
+
+		Set<Integer> keys = map.keySet();
+		List<Integer> keyList = new ArrayList<>(keys);
+		Collections.sort(keyList);
+		System.out.println("keyList = " + keyList);
+
+		Collection<Book> values = map.values();
+		List<Book> bookList = new ArrayList<>(values);
+		Collections.sort(bookList);
+		System.out.println("bookList = " + bookList);
+		Collections.reverse(bookList);
+		System.out.println("bookList = " + bookList);
 	}
+
 }
