30f0382d5676e9db99f0786f6609abcae5084ce4
indiflex <indiflex1@gmail.com>
2025-07-15 10:31:38 +0900

    methods & fields

--- java/basic/src/lambda/Reflection.java
+++ java/basic/src/lambda/Reflection.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@ToString(callSuper = true)
 public class Reflection extends Parent {
 	private String name;
 
@@ -18,23 +20,13 @@ public class Reflection extends Parent {
 	}
 
 	public static void main(String[] args) {
-		Reflection inst = new Reflection(1, "Hong");
-		System.out.println("inst.getName() = " + inst.getName());
 
-		try {
-			Class<?> rc2 = Class.forName("lambda.Reflection");
-			System.out.println("rc2 = " + rc2);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace(System.out);
-		}
-
-		Class<Reflection> rc = Reflection.class;
-		System.out.println("rc = " + rc);
 	}
 }
 
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 class Parent {
 	private int id;
 }
