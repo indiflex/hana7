151ff4d4b9db0406eb32b7a50ba9bef858a4a8d9
indiflex <indiflex1@gmail.com>
2025-07-15 14:13:57 +0900

    annotation

--- java/basic/src/lambda/Reflection.java
+++ java/basic/src/lambda/Reflection.java
@@ -14,6 +14,8 @@ import lombok.ToString;
 public class Reflection extends Parent {
 	private String name;
 	private Integer deptId;
+	private Double addr;
+	private Boolean isGuest;
 
 	public Reflection(int id, String name) {
 		super(id);
@@ -21,7 +23,10 @@ public class Reflection extends Parent {
 	}
 
 	public static void main(String[] args) {
-
+		Reflection r = new Reflection();
+		System.out.println("r-before = " + r);
+		Reflects.makeNotNullFields(r);
+		System.out.println("r-after = " + r);
 	}
 }
 
