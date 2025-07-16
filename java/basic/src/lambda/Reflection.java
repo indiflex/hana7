c52a1aad08a4734a810d3121fda825c75a1a8e20
indiflex <indiflex1@gmail.com>
2025-07-16 10:03:11 +0900

    Validator Annotations

--- java/basic/src/lambda/Reflection.java
+++ java/basic/src/lambda/Reflection.java
@@ -1,5 +1,11 @@
 package lambda;
 
+import java.util.Arrays;
+
+import lambda.annotaions.In;
+import lambda.annotaions.Max;
+import lambda.annotaions.Min;
+import lambda.annotaions.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +18,18 @@ import lombok.ToString;
 @Setter
 @ToString(callSuper = true)
 public class Reflection extends Parent {
+	@Min(value = 3, msg = "3글자 이상 입력하세요!")
+	@In({"Hong", "Kim", "Lee", "Choi"})
 	private String name;
+
+	@NotNull()
+	@Min(value = 5, msg = "5보다는 커야합니다!")
+	@Max(value = 10, msg = "10보다 작아야합니다!")
 	private Integer deptId;
+
+	@NotNull("필수값이에요")
+	@Min(3)
+	@Max(15)
 	private Double addr;
 	private Boolean isGuest;
 
@@ -22,11 +38,12 @@ public class Reflection extends Parent {
 		this.name = name;
 	}
 
-	public static void main(String[] args) {
-		Reflection r = new Reflection();
-		System.out.println("r-before = " + r);
+	public static void main(String[] args) throws IllegalAccessException {
+		Reflection r = new Reflection(5, null);
+		System.out.println(Arrays.toString(Reflects.vaidate(r)));
+		// System.out.println("r-before = " + r);
 		Reflects.makeNotNullFields(r);
-		System.out.println("r-after = " + r);
+		// System.out.println("r-after = " + r);
 	}
 }
 
--- java/basic/src/lambda/Reflects.java
+++ java/basic/src/lambda/Reflects.java
@@ -1,6 +1,17 @@
 package lambda;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import lambda.annotaions.IllegalAnnotationException;
+import lambda.annotaions.In;
+import lambda.annotaions.Max;
+import lambda.annotaions.Min;
+import lambda.annotaions.NotNull;
 
 public class Reflects {
 	public static void makeNotNullFields(Object obj) {
@@ -21,5 +32,86 @@ public class Reflects {
 				e.printStackTrace(System.out);
 			}
 		}
+	}
+
+	public static String[] vaidate(Object obj) throws IllegalAccessException {
+		MessageCollector messageCollector = new MessageCollector();
+
+		for (Field f : obj.getClass().getDeclaredFields()) {
+			f.setAccessible(true);
+			Object val = f.get(obj);
+			String fname = f.getName();
+
+			if (val == null && f.isAnnotationPresent(NotNull.class)) {
+				NotNull notNull = f.getAnnotation(NotNull.class);
+				messageCollector.addMessage(fname, notNull.value());
+			}
+
+			if (f.isAnnotationPresent(Min.class)) {
+				Min min = f.getAnnotation(Min.class);
+				var len = 0.0;
+				if (f.getType() == String.class) {
+					len = val == null ? 0 : ((String)val).length();
+				} else {
+					len = val == null ? 0 : (double)val;
+				}
+
+				if (len < min.value()) {
+					messageCollector.addMessage(fname, min.msg().formatted(min.value()));
+				}
+			}
+
+			if (f.isAnnotationPresent(Max.class)) {
+				Max max = f.getAnnotation(Max.class);
+				var len = 0.0;
+				if (f.getType() == String.class) {
+					len = val == null ? 0 : ((String)val).length();
+				} else {
+					len = val == null ? 0 : (double)val;
+				}
+
+				if (len > max.value()) {
+					messageCollector.addMessage(fname, max.msg().formatted(max.value()));
+				}
+			}
+
+			if (f.isAnnotationPresent(In.class)) {
+				In in = f.getAnnotation(In.class);
+				if (f.getType() != String.class)
+					throw new IllegalAnnotationException("Only use In annotaion for String field!!");
+
+				if (val == null || !Arrays.asList(in.value()).contains((String)val)) {
+					messageCollector.addMessage(fname, in.msg().formatted(Arrays.toString(in.value())));
+				}
+			}
+		}
+
+		return messageCollector.toStringArray();
+	}
+
+}
+
+class MessageCollector {
+	Map<String, List<String>> collectedMessage = new HashMap<>();
+
+	public void addMessage(String key, String message) {
+		List<String> messages;
+		if (collectedMessage.containsKey(key)) {
+			messages = collectedMessage.get(key);
+		} else {
+			messages = new ArrayList<>();
+			collectedMessage.put(key, messages);
+		}
+		messages.add(message);
+	}
+
+	public String[] toStringArray() {
+		String[] results = new String[collectedMessage.size()];
+		int idx = 0;
+		for (Map.Entry<String, List<String>> entry : collectedMessage.entrySet()) {
+			results[idx++] = entry.getKey() + ":" + entry.getValue();
+		}
+
+		return results;
 	}
 }
