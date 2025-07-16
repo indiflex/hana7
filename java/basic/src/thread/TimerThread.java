91a163cbab5347b51605548961ebdf3cbfae0b60
indiflex <indiflex1@gmail.com>
2025-07-16 14:28:38 +0900

    interrupt & join

--- java/basic/src/thread/TimerThread.java
+++ java/basic/src/thread/TimerThread.java
@@ -17,21 +17,27 @@ public class TimerThread extends Thread {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
+				System.out.println(name + " thread's interrupted!!!");
 				throw new RuntimeException(e);
 			}
 		}
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+		System.out.println("Thread.currentThread().getName() = " + Thread.currentThread());
 
 		TimerThread timer1 = new TimerThread("Timer1");
-		// TimerThread timer2 = new TimerThread("Timer2");
+		TimerThread timer2 = new TimerThread("Timer2");
+		timer1.setPriority(Thread.MIN_PRIORITY);
+		timer2.setPriority(Thread.MAX_PRIORITY);
 		timer1.start();
-		// timer2.start();
+		timer2.start();
 
 		for (int i = 0; i < 5; i++) {
 			Thread.sleep(1000);
+			if (i == 1)
+				timer2.join();
+			// timer2.interrupt();
 			System.out.println(i + "번 main 쓰레드.");
 		}
 	}
