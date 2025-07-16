d1c283b526f6409f596be343c437c77911106acb
indiflex <indiflex1@gmail.com>
2025-07-16 13:50:20 +0900

    thread

---
+++ java/basic/src/thread/TimerRunnable.java
@@ -0,0 +1,15 @@
+package thread;
+
+public class TimerRunnable implements Runnable {
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
