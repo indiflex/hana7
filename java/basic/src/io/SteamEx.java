c292e2cad48ec3a8b6af12dd07d0a08539c2d1cb
indiflex <indiflex1@gmail.com>
2025-07-17 11:48:17 +0900

    file read & right at once

--- java/basic/src/io/SteamEx.java
+++ java/basic/src/io/SteamEx.java
@@ -1,14 +1,66 @@
 package io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
 public class SteamEx {
+	public static final String SRC_FILE = "./tmp.txt";
+	public static final String DEST_FILE = "./tmp2.txt";
+
 	public static void main(String[] args) throws IOException {
 		// sysout();
 		// bufFos();
+		// sysinout();
 
+		long startTime = System.currentTimeMillis();
+		// oneByte();
+		// bufByte();
+		buffed();
+		System.out.printf("EllapseTime is %d%n", (System.currentTimeMillis() - startTime));
+	}
+
+	private static void buffed() {
+		try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(SRC_FILE));
+			 BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(DEST_FILE))) {
+			while (bis.available() > 0) {
+				bos.write(bis.read());
+			}
+		} catch (IOException e) {
+			e.printStackTrace(System.out);
+		}
+	}
+
+	private static void bufByte() {
+		try (FileInputStream fis = new FileInputStream(SRC_FILE);
+			 FileOutputStream fos = new FileOutputStream(DEST_FILE)) {
+			int c = -1;
+			byte[] buf = new byte[512];
+			while ((c = fis.read(buf)) != -1) {
+				fos.write(buf, 0, c);
+				fos.write('\n');
+			}
+		} catch (IOException e) {
+			e.printStackTrace(System.out);
+		}
+	}
+
+	private static void oneByte() {
+		try (FileInputStream fis = new FileInputStream(SRC_FILE);
+			 FileOutputStream fos = new FileOutputStream(DEST_FILE)) {
+			int c = -1;
+			while ((c = fis.read()) != -1) {
+				fos.write(c);
+			}
+		} catch (IOException e) {
+			e.printStackTrace(System.out);
+		}
+	}
+
+	private static void sysinout() throws IOException {
 		int b = 0;
 		int len = 0;
 		int[] buf = new int[100];
@@ -33,7 +85,7 @@ public class SteamEx {
 		// fos.close();
 		int BS = 512;
 		long start = System.currentTimeMillis();
-		try (FileOutputStream fos = new FileOutputStream("./tmp.txt")) {
+		try (FileOutputStream fos = new FileOutputStream(SRC_FILE)) {
 			byte[] buf = new byte[BS];
 			int idx = 0;
 			for (int i = 0; i < 2 * 1024 * 1024; i++) {
