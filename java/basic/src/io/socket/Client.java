1c44e9263f05425195155996f934c79f03500e3b
indiflex <indiflex1@gmail.com>
2025-07-18 10:45:27 +0900

    socket

---
+++ java/basic/src/io/socket/Client.java
@@ -0,0 +1,23 @@
+package io.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+	public static final int PORT = 9999;
+	public static void main(String[] args) {
+		try (Socket socket = new Socket("localhost", PORT)) {
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			System.out.println("server: " + in.readUnsignedByte());
+			out.writeUTF("Hi~");
+
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			System.out.println("서버와 연결이 종료되었습니다.");
+		}
+	}
+}
