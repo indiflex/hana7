cbcfa7af6522f8bf7f1f40c0f1b7051d7488cae6
indiflex <indiflex1@gmail.com>
2025-07-22 16:35:41 +0900

    HelloController - ApplicationContext version

--- spring/demo/src/main/java/com/hana7/demo/HelloController.java
+++ spring/demo/src/main/java/com/hana7/demo/HelloController.java
@@ -1,12 +1,7 @@
 package com.hana7.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
-	@GetMapping("/hello")
 	public String hello(String name) {
-		return "Hello " + name + "!";
+		return "Hello " + name + "~";
 	}
 }
