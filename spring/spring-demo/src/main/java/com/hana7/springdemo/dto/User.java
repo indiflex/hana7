0b6259f32eb169f69403ad111c93d9ed134cdf11
indiflex <indiflex1@gmail.com>
2025-07-23 14:48:52 +0900

    user controller

--- spring/spring-demo/src/main/java/com/hana7/springdemo/dto/User.java
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/dto/User.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,6 +13,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
 public class User {
 	private int id;
 
