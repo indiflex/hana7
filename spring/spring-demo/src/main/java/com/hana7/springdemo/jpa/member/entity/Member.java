1bcc5e3d6a11db0c72b161ec2cda333a379e2011
indiflex <indiflex1@gmail.com>
2025-07-24 16:44:28 +0900

    member

---
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/member/entity/Member.java
@@ -0,0 +1,49 @@
+package com.hana7.springdemo.jpa.member.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 31, nullable = false)
+	@ColumnDefault("'Guest'")
+	private String nickname;
+
+	@Column(nullable = false)
+	@Email
+	private String email;
+
+	@CreationTimestamp
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+	private LocalDateTime updatedAt;
+
+	@Transient
+	private int auth;
+}
