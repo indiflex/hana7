b260bd9de9c9b199755c9c0a62f35c536d3cbbd3
indiflex <indiflex1@gmail.com>
2025-07-29 15:49:18 +0900

    Reply test code

--- spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/entity/Reply.java
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/entity/Reply.java
@@ -8,8 +8,21 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Builder
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = true)
 public class Reply extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
