ff89e15a0c9ee6d2c829599e9bc5ff9846066929
indiflex <indiflex1@gmail.com>
2025-08-04 09:47:40 +0900

    swagger

--- spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/controller/MemberController.java
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/controller/MemberController.java
@@ -12,6 +12,11 @@ import com.hana7.springdemo.jpa.dto.MemberDTO;
 import com.hana7.springdemo.jpa.dto.SearchCond;
 import com.hana7.springdemo.jpa.service.MemberService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/members")
 public class MemberController {
@@ -22,12 +27,30 @@ public class MemberController {
 	}
 
 	@GetMapping()
-	List<MemberDTO> findMembers(SearchCond searchCond) {
+	@Tag(name = "회원 목록", description = "회원 페이징 목록")
+	@Operation(summary = "/members?page=1...", description = "회원목록")
+	List<MemberDTO> findMembers(
+		@Parameter(example = """
+			{
+				"page": 1,
+				"size": 5,
+				"searchNickname": "x",
+				"searchEmail:: null,
+				"sortField": "id",
+				"sortDirection" : "asc"
+			}
+			""")
+		SearchCond searchCond) {
 		System.out.println("searchCond = " + searchCond.getPager());
 		return service.findAll(searchCond);
 	}
 
 	@GetMapping("{id}")
+	@Tag(name = "회원 상세")
+	@Operation(summary = "/members/2", description = "회원 작성된 글 포함")
+	@Parameters({
+		@Parameter(name = "id", description = "회원 번호", example = "2"),
+	})
 	MemberDTO getMember(@PathVariable Long id) {
 		return service.findOne(id);
 	}
