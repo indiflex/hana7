70cf2a78352fdc7c0240dd0c77ba0b55b07331f8
indiflex <indiflex1@gmail.com>
2025-09-01 08:13:12 +0900

    x4

--- next/my-app/next.config.ts
+++ next/my-app/next.config.ts
@@ -25,7 +25,7 @@ const nextConfig: NextConfig = {
     //       });
     //     }
     //   }
-    //   return config;
+    return config;
   },
   // transpilePackages: ['styled-jsx', '@swc/helpers'],
 
